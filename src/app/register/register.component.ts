e0bfb64a6ed7d96621be380936c063a7d81c4643
Tim Weilbach <tim97.weilbach@gmx.de>
2020-12-10 23:43:37 +0100

    add a validator for password

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { RegisterService } from '../_services';
 import { first } from 'rxjs/operators';
 import { Router } from '@angular/router';
@@ -14,6 +19,42 @@ export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   registerError: boolean = false;
   registerErrorMessage: string = 'Verify your inputs';
+  passwordErrorMessage: string =
+    'Please enter a password with minimum one upper case and a special character';
+
+  hasUpperCase(input: string) {
+    for (let i = 0; i < input.length; i++) {
+      if (input[i] === input[i].toUpperCase()) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  hasSpecialCharacter(input: string) {
+    let specialCharacters = [
+      '!',
+      '@',
+      '#',
+      '$',
+      '%',
+      '^',
+      '&',
+      '*',
+      '(',
+      ')',
+      '-',
+      '+',
+    ];
+    for (let i = 0; i < input.length; i++) {
+      for (let j = 0; j < specialCharacters.length; j++) {
+        if (input[i] === specialCharacters[j]) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 
   hidePassword = true;
 
@@ -40,13 +81,17 @@ export class RegisterComponent implements OnInit {
       return;
     }
 
-    // try to login user
     const controls = this.registerForm.controls;
 
     const lastName = controls.lastName.value;
     const firstName = controls.firstName.value;
     const email = controls.email.value;
     const password = controls.password.value;
+
+    if (!this.hasUpperCase(password) || !this.hasSpecialCharacter(password)) {
+      this.registerForm.controls['password'].setErrors({ incorrect: true });
+      return;
+    }
 
     this.registerService
       .register(lastName, firstName, email, password)
@@ -65,22 +110,38 @@ export class RegisterComponent implements OnInit {
   }
 
   getFirstNameErrorMessage(): string {
+    const firstName = this.registerForm.controls.firstName;
+    if (firstName.hasError('required')) {
+      return 'Please enter your first name';
+    }
     return '';
   }
   getLastNameErrorMessage(): string {
+    const lastName = this.registerForm.controls.lastName;
+    if (lastName.hasError('required')) {
+      return 'Please enter your last name';
+    }
     return '';
   }
   getEmailErrorMessage(): string {
+    const email = this.registerForm.controls.email;
+
+    if (email.hasError('required')) {
+      return 'You must enter a email';
+    } else if (email.hasError('email')) {
+      return 'Not a valid email';
+    }
+
     return '';
   }
   getPasswordErrorMessage(): string {
-    return '';
+    return this.passwordErrorMessage;
   }
   getPasswordRepeatErrorMessage(): string {
     return '';
   }
   getRegisterErrorMessage(): string {
-    return '';
+    return this.registerErrorMessage;
   }
 
   ngOnInit(): void {
